389ad3bbf9dfc6a4d30c2f169e253145245895fc
wilzlerr <william.weng@hotmail.com>
2017-03-05 16:21:01 +1100

    updated two strings.c

--- hackerrank/twostrings.c
+++ hackerrank/twostrings.c
@@ -4,33 +4,29 @@
 #include <stdlib.h>
 
 int main() {
-    int numPairs = i = 0;
-    char* word1, word2;
+    int numPairs, i = 0;
     int alphabet[26] = {0};
     scanf("%d", &numPairs);
-    
-    int bytes_read;
-    int nbytes = 100;
-    char *my_string;
-    my_string = (char *) malloc (nbytes + 1);
-    
+    char c;
+    scanf("%c", &c);
+
     for(i = 0; i < numPairs; ++i)
     {
-
-        bytes_read = getline (&word1, &nbytes, stdin);
-        bytes_read = getline (&word2, &nbytes, stdin);
-        for(i = 0; word1[i] != 0; ++i)
+        for(i = 0; c != '\n'; ++i)
         {
-            alphabet[word1[i] - 'a']++;
+            alphabet[c - 'a']++;
+            scanf("%c", &c);
         }
-        for(i = 0; word2[i] != 0; ++i) {
-            if(alphabet[word2[i] - 'a'] != 0) {
+        for(i = 0; c != '\n'; ++i)
+        {   
+            if(alphabet[c - 'a'] != 0) {
                 printf("YES\n");
                 break;
             } else {
                 printf("NO\n");
             }
+            scanf("%c", &c);
         }
     }
-  return 0;
+    return 0;
 }
